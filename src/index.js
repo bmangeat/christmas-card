296a62d93ab0125d9cbe3ed0b5f45c1b08075c42
bmangeat <brice.mangeat@gmail.com>
2020-12-24 00:05:52 +0100

    :sparkles: Add content adapted for IE

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import './styles.css'
 import Scene from "./Scene"
 
-setTimeout(window.scene = new Scene(), 200000)
+
+const msieversion = () => {
+
+    let ua = window.navigator.userAgent;
+    let msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+    {
+        document.querySelector('.user-ie').style.opacity = 1
+        document.querySelector('.main').style.backgroundColor = '#212121'
+        let video = document.querySelector('.user-ie')
+        video.style.opacity = 1
+        video[0].onended = function (  ) {
+            this.load()
+            this.play()
+        }
+
+    }
+    else  // If another browser, return 0
+    {
+       window.scene = new Scene()
+    }
+
+    return false;
+}
+
+msieversion()
