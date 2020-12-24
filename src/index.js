f05efcf51ee62148fe9b293b415042005ef44f82
bmangeat <brice.mangeat@gmail.com>
2020-12-24 10:55:24 +0100

    :bug: Edit internet explorer

--- src/index.js
+++ src/index.js
@@ -2,25 +2,6 @@ import './styles.css'
 import Scene from "./Scene"
 
 
-const msieversion = () => {
-
-    window.scene = new Scene()
+window.scene = new Scene()
 
 
-    let ua = window.navigator.userAgent;
-    let msie = ua.indexOf("MSIE ");
-
-    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
-    {
-
-
-    }
-    else  // If another browser, return 0
-    {
-        document.querySelector('.user-ie').style.opacity = 0
-    }
-
-    return false;
-}
-
-msieversion()
