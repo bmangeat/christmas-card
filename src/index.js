4cbe4800a4839071ac8454e7655a94b89c2cf8c9
bmangeat <brice.mangeat@gmail.com>
2020-12-24 10:48:48 +0100

    :bug: Edit for internet explorer

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ import Scene from "./Scene"
 
 const msieversion = () => {
 
+    window.scene = new Scene()
+
+
     let ua = window.navigator.userAgent;
     let msie = ua.indexOf("MSIE ");
 
     if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
     {
-        console.log('je suis sur IE')
-        document.querySelector('.user-ie').style.opacity = 1
-        document.querySelector('.main').style.backgroundColor = '#212121'
-        let container_video = document.querySelector('.user-ie')
-        container_video.style.opacity = 1
+
 
     }
     else  // If another browser, return 0
     {
-       window.scene = new Scene()
+        document.querySelector('.user-ie').style.opacity = 0
     }
 
     return false;
